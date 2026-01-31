6d5ac7958eeafc470a3a81d46acbc93ec9e5f25d
GaelicThunder <48930397+GaelicThunder@users.noreply.github.com>
2026-01-31 12:46:56 +0100

    Delete chat event mock file

--- src/main/java/net/hytalegame/api/event/player/PlayerChatEvent.java
+++
@@ -1,7 +0,0 @@
-package net.hytalegame.api.event.player;
-import net.hytalegame.api.entity.Player;
-// MOCK CLASS
-public class PlayerChatEvent {
-    public Player getPlayer() { return new Player("MockUser"); }
-    public String getMessage() { return "Hello"; }
-}
